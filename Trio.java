c25c642f53c1beb4c7303717cebdfaafc4ec78e3
aAlvey <asher.alvey@stu.daviess.kyschools.us>
2025-04-07 17:30:27 +0000

    idek

--- Trio.java
+++ Trio.java
@@ -1,1 +1,16 @@
 //Write your answer here
+public class Trio{
+
+
+public Trio (String sand, sal, dr){
+    sandwich = sand;
+
+    salad = sal;
+
+    drink = dr;
+}
+
+public String getName(){
+
+}
+}
